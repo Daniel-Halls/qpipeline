77c15cd556797b5110bfeb8d6d38bec70c48637b
wmda <daniel.halls@nottingham.ac.uk>
2025-03-31 13:24:59 +0100

    addedi n pipeline handler

---
+++ qpipeline/base/Qpipeline.py
@@ -0,0 +1,32 @@
+class Qpipeline:
+    """
+    Main class of qpipeline
+
+    Determines what part of the pipeline
+    should be ran.
+
+    Usage
+    -----
+    pipeline = Qpipeline()
+    pipeline.qpipeline_handler(args['command'], args)
+    """
+
+    def __init__(self):
+        pass
+
+    def setup(self, **kwargs):
+        from qpipeline.qunex_setup.study_setup import set_up_qunex_study
+
+        set_up_qunex_study(kwargs)
+
+    def qpipeline_handler(self, command: str, args: str):
+        """
+        Method to determine what part of the pipeline
+        should be ran based on user input
+
+        Parameters
+        ----------
+
+        """
+        func = getattr(self, command)
+        func(**{key: value for key, value in args.items() if key != "command"})
