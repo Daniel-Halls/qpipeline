e31fcb6c9bf2dd7496b39eba11b16eea76d19445
wmda <daniel.halls@nottingham.ac.uk>
2025-04-11 14:14:45 +0100

    added in singit handler

--- qpipeline/__main__.py
+++ qpipeline/__main__.py
@@ -1,9 +1,11 @@
 from qpipeline.base.args import qpipeline_args
 from qpipeline.base.setup import set_environment
 from qpipeline.base.Qpipeline import Qpipeline
+from qpipeline.base.signit import Signit_handler
 
 
 def main():
+    Signit_handler()
     args = qpipeline_args()
     set_environment()
     pipeline = Qpipeline()
