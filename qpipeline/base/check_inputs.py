11903d2f88bba0adee3135e2a10ad592b4dc2870
Daniel Halls <daniel.halls@nottingham.ac.uk>
2025-04-28 16:55:31 +0100

    Now does checking of inputs and errors if not given

--- qpipeline/base/check_inputs.py
+++ qpipeline/base/check_inputs.py
@@ -2,30 +2,61 @@ import os
 from qpipeline.base.utils import error_and_exit
 
 
-def make_paths_absolute(args: dict, paths_to_make_abs: list) -> str:
+def check_paths_absolute(paths_to_check: list) -> None:
     """
-    Function to turn paths
-    into absolute paths
+    Function to check paths
+    are absolute paths
 
     Parameters
     ----------
-    args: dict
-       cmd line arguments
-    paths_to_make_abs: list
-       list of keys to make
+    paths_to_check: list
+       list of paths to
+       check that they are absolute
+
+    Returns
+    -------
+    None
     """
-    for path in paths_to_make_abs:
-        args[path] = os.path.abspath(path)
-    return args
+    for path in paths_to_check:
+        error_and_exit(
+            os.path.isabs(path),
+            f"{path} is not absolute path. Qunex needs absolute paths",
+        )
 
 
 def check_folders_exist(paths: list) -> None:
+    """
+    Function to check that folders exist
+
+    Parameters
+    ----------
+    paths: list
+       list of paths to
+       check that they exist
+
+    Returns
+    -------
+    None
+    """
     for path in paths:
         if not os.path.exists(path):
             error_and_exit(False, f"{path} does not exist. Please check input")
 
 
 def check_complusory_arguments(args: dict) -> None:
+    """
+    Function to check that complusory arguments
+    have been given.
+
+    Parameters
+    ----------
+    args: dict
+        dictionary of cmd arguments
+
+    Returns
+    -------
+    None
+    """
     compulsory = ["study_folder", "id"]
     if args["command"] == "setup":
         compulsory.append("raw_data")
@@ -36,6 +67,19 @@ def check_complusory_arguments(args: dict) -> None:
 
 
 def check_bids_folder(bids_dir: str) -> None:
+    """
+    Function to do the very basic
+    bids check that qunex needs.
+
+    Parameters
+    ----------
+    bids_dir: str
+       path to bids directory
+
+    Returns
+    -------
+    None
+    """
     anat_dir = os.path.join(bids_dir, "anat")
     dwi_dir = os.path.join(bids_dir, "dwi")
     anat_data = True if (os.path.exists(anat_dir) and os.listdir(anat_dir)) else False
@@ -61,11 +105,12 @@ def check_input(args: dict) -> None:
     None
     """
     check_complusory_arguments(args)
-    subselect_keys = (
-        ["study_folder", "raw_data"] if args["raw_data"] else ["study_folder"]
+    folder_paths = (
+        [args["study_folder"], args["raw_data"]]
+        if args["raw_data"]
+        else args["study_folder"]
     )
-    paths_to_check = {key: args[key] for key in subselect_keys if key in subselect_keys}
-    args = make_paths_absolute(args, list(paths_to_check.keys()))
-    check_folders_exist(list(paths_to_check.valeus()))
+    check_paths_absolute(folder_paths)
+    check_folders_exist(folder_paths)
     if args["command"] == "setup":
         check_bids_folder(args["raw_data"])
